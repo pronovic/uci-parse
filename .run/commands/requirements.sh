c35fae60923b9442b67807a9a74672a2501f3628
Kenneth J. Pronovici <pronovic@users.noreply.github.com>
2022-10-23 18:58:29 -0500

    Refactor the run script and pre-commit hooks to reduce duplication (#23)

---
+++ .run/commands/requirements.sh
@@ -0,0 +1,46 @@
+# vim: set ft=bash ts=3 sw=3 expandtab:
+# Generate a requirements.txt file in the docs directory, for use by readthedocs.io
+
+# Unfortunately, we can't just rely on Poetry here.  Poetry wants to generate a
+# file that includes our project's lowest compatible version of Python, but
+# readthedocs.io can only build with older versions (v3.7 as of this writing,
+# even though it's years out of date).  So, we need to modify the generated
+# result to make readthedocs.io happy.
+#
+# The "solution" is to replace whatever lowest python version Poetry generated
+# with "3.7", and hope for the best. That seems to have been working so far,
+# but we may eventually run into a dependency that simply doesn't exist for
+# Python 3.7.
+
+command_requirements() {
+   echo -n "Generating docs/requirements.txt..."
+
+   poetry self add --quiet poetry-plugin-export
+   if [ $? != 0 ]; then
+      echo ""
+      echo "*** Failed to install Poetry plugin: poetry-plugin-export"
+      exit 1
+   fi
+
+   poetry export --format=requirements.txt --without-hashes --with dev --output=docs/requirements.txt
+   if [ $? != 0 ]; then
+      echo ""
+      echo "*** Failed to export docs/requirements.txt"
+      exit 1
+   fi
+
+   REPLACEMENT='s|python_version >= "3\.[0-9][0-9]*"|python_version >= "3.7"|g'
+   sed --version 2>&1 | grep -iq "GNU sed"
+   if [ $? = 0 ]; then
+      # GNU sed accepts a bare -i and assumes no backup file
+      sed -i "$REPLACEMENT" docs/requirements.txt
+   else
+      # BSD sed requires you to set an empty backup file extension
+      sed -i "" "$REPLACEMENT" docs/requirements.txt
+   fi
+
+   run_command dos2unix docs/requirements.txt
+
+   echo "done"
+}
+
---
+++ .run/tasks/suite.sh
@@ -0,0 +1,13 @@
+# vim: set ft=bash sw=3 ts=3 expandtab:
+
+help_suite() {
+   echo "- run suite: Run the complete test suite, as for the GitHub Actions CI build"
+}
+
+task_suite() {
+   run_task install
+   run_task checks
+   run_task test -c
+   run_task docs
+}
+
