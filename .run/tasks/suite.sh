9233e890eabdfeaebeea98970807fa9a0ae96484
Kenneth J. Pronovici <pronovic@ieee.org>
2025-09-01 00:02:47 -0500

    Add a new 'suite -f' command to run faster subset of the suite

--- .run/tasks/suite.sh
+++ .run/tasks/suite.sh
@@ -3,15 +3,21 @@
 
 help_suite() {
    echo "- run suite: Run the complete test suite, as for the GitHub Actions CI build"
+   echo "- run suite -f: Run a faster version of the test suite, ommitting some steps"
 }
 
 task_suite() {
-   run_command pythonversion --short
-   run_task install
-   run_task checks
-   run_task build
-   run_task test -c
-   run_task docs
-   run_command pythonversion
+   if [ "$1" == "-f" ]; then
+      run_command pythonversion --short
+      run_task checks
+      run_task test
+   else
+      run_command pythonversion --short
+      run_task install
+      run_task checks
+      run_task build
+      run_task test -c
+      run_task docs
+      run_command pythonversion
+   fi
 }
-
